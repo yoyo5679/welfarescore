35ef247674060a0a50319b731d202c2221e3c243
yoyo5679 <62796904+yoyo5679@users.noreply.github.com>
2026-02-20 18:49:56 +0900

    Delete query_bokjiro_cats.py

--- query_bokjiro_cats.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://www.bokjiro.go.kr/ssis-tbu/twataa/wlfareInfo/moveTWAT52005M.do"
-try:
-    response = requests.get(url, verify=False, timeout=10)
-    print("Length:", len(response.text))
-except Exception as e:
-    print("Error:", e)
